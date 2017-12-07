02419af4592435a9c2cf153662cf67a2b3b3ea21
JuanFranCoder <jf_vk9@hotmail.com>
2017-12-07 05:38:33 +0100

    Miembros listaFamiliar

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,18 +1,18 @@
 'use strict';
 
-module.exports = function(Listafamiliar) {
-    Listafamiliar.beforeRemote('create', function(context, listafamiliar, next){
+module.exports = function (Listafamiliar) {
+    Listafamiliar.beforeRemote('create', function (context, listafamiliar, next) {
         context.args.data.owner = context.req.accessToken.userId;
         next();
     });
-    Listafamiliar.afterRemote('create', function(context, listafamiliar, next) { 
+    Listafamiliar.afterRemote('create', function (context, listafamiliar, next) {
         // obtener id de la lista que se ha creado y guardado en la BD
-        var id_listaFamiliar=listafamiliar.id;
+        var id_listaFamiliar = listafamiliar.id;
         // buscar ne la BD la instancia del usuario autenticado 
-        var Usuario=Listafamiliar.app.models.Usuario;
+        var Usuario = Listafamiliar.app.models.Usuario;
         // también se puede usar listafamiliar.owner dentro del paréntesis
-        Usuario.findById(context.req.accessToken.userId, function(err, usuario){
-            if(err) next(err);
+        Usuario.findById(context.req.accessToken.userId, function (err, usuario) {
+            if (err) next(err);
             usuario.listaFamiliarId = id_listaFamiliar;
             usuario.save();
         });
@@ -24,16 +24,42 @@ module.exports = function(Listafamiliar) {
     * @param {object} req Objeto que contiene objeto request
     * @param {Function(Error, object)} callback
     */
-   
-    Listafamiliar.prototype.solicitar = function(req, callback) {
+
+    Listafamiliar.prototype.solicitar = function (req, callback) {
         var listaFamiliar = this;
-        listaFamiliar.solicitudes.add(req.accessToken.userId, function(err){
-            if(err) callback(err);
+        listaFamiliar.solicitudes.add(req.accessToken.userId, function (err) {
+            if (err) callback(err);
             var solicitud = {
-                listaFamiliarId:listaFamiliar.id,
-                usuarioId:req.accessToken.userId
+                listaFamiliarId: listaFamiliar.id,
+                usuarioId: req.accessToken.userId
             }
             callback(null, solicitud);
         });
-    };   
+    };
+
+
+    /**
+ * Muestra los usuarios de la listaFamiliar a la que pertenezco
+ * @param {object} context Objeto context
+ * @param {Function(Error, array)} callback
+ */
+
+    Listafamiliar.mostrarUsuarios = function (context, callback) {
+        var Usuario = Listafamiliar.app.models.Usuario;
+        var idUsuario = context.req.accessToken.userId;
+
+        Usuario.findById(idUsuario, function(err, usuario){
+            if(err)callback(err);
+            var id_listaFamiliar_usuario = usuario.listaFamiliarId;
+            console.log("El id de la lista familiar del usuario logueado es: "+id_listaFamiliar_usuario);
+            Usuario.find({where:{listaFamiliarId:id_listaFamiliar_usuario}}, function(err, listaUsuarios){
+                if(err)callback(err);
+                console.log(listaUsuarios);
+                callback(null, listaUsuarios);
+            });
+        });
+
+        
+    };
+
 };
